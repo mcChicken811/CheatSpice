fd1787ad878c798afe51e8444c84efc2574d5249
username <useremail>
2026-03-21 23:38:36 +1100

    re add the sample circuit

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -5,16 +5,32 @@ public class Program {
         /* setting up the circuit */
         Circuit circuit = new Circuit();
 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 4; i++) {
             circuit.addNode();
         }
 
-        circuit.addDCVoltageSource(1);
+        circuit.addResistor(6.0);
+        circuit.getNewestComponent().connectToNode(circuit.getNode(0), 1);
+        circuit.getNewestComponent().connectToNode(circuit.getNode(3), 2);
+
+        circuit.addResistor(12.0);
         circuit.getNewestComponent().connectToNode(circuit.getNode(0), 1);
         circuit.getNewestComponent().connectToNode(circuit.getNode(1), 2);
 
-        circuit.addResistor(5);
+        circuit.addResistor(12.0);
+        circuit.getNewestComponent().connectToNode(circuit.getNode(3), 1);
+        circuit.getNewestComponent().connectToNode(circuit.getNode(2), 2);
+
+        circuit.addResistor(6.0);
+        circuit.getNewestComponent().connectToNode(circuit.getNode(1), 1);
+        circuit.getNewestComponent().connectToNode(circuit.getNode(2), 2);
+
+        circuit.addDCVoltageSource(20.0);
         circuit.getNewestComponent().connectToNode(circuit.getNode(0), 1);
+        circuit.getNewestComponent().connectToNode(circuit.getNode(2), 2);
+
+        circuit.addWire();
+        circuit.getNewestComponent().connectToNode(circuit.getNode(3), 1);
         circuit.getNewestComponent().connectToNode(circuit.getNode(1), 2);
 
         System.out.println(circuit.toString());
